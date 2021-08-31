045d1c28797ea695766c48e9b2f360f2c2bd333a
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-08-31 09:54:35 +0300

    Fix idling resources

--- app/src/androidTest/java/io/github/wykopmobilny/tests/BaseActivityTest.kt
+++ app/src/androidTest/java/io/github/wykopmobilny/tests/BaseActivityTest.kt
@@ -8,6 +8,7 @@ import io.github.wykopmobilny.storage.api.LoggedUserInfo
 import io.github.wykopmobilny.storage.api.UserSession
 import io.github.wykopmobilny.tests.responses.callsOnAppStart
 import io.github.wykopmobilny.tests.rules.CleanupRule
+import io.github.wykopmobilny.tests.rules.DispatcherIdlerRule
 import io.github.wykopmobilny.tests.rules.IdlingResourcesRule
 import io.github.wykopmobilny.tests.rules.MockWebServerRule
 import io.github.wykopmobilny.ui.modules.mainnavigation.MainNavigationActivity
@@ -22,6 +23,7 @@ abstract class BaseActivityTest {
     @get:Rule
     val rules: RuleChain = RuleChain.outerRule(IdlingResourcesRule())
         .around(CleanupRule())
+        .around(DispatcherIdlerRule())
         .around(mockWebServerRule)
 
     protected fun logUserIn() = runBlocking {
