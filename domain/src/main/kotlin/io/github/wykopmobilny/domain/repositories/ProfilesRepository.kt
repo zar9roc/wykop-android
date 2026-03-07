bb2da66a6a8c771c9335ead2f4554c14b50177b5
Zarroc <starygwizd3@gmail.com>
2026-03-07 23:31:45 +0100

    Migracja ProfilesRepository (domain) na v3

--- domain/src/main/kotlin/io/github/wykopmobilny/domain/repositories/ProfilesRepository.kt
+++ domain/src/main/kotlin/io/github/wykopmobilny/domain/repositories/ProfilesRepository.kt
@@ -1,67 +1,36 @@
 package io.github.wykopmobilny.domain.repositories
 
-import io.github.wykopmobilny.api.endpoints.ProfileRetrofitApi
-import io.github.wykopmobilny.api.responses.ObserveStateResponse
+import io.github.wykopmobilny.api.endpoints.v3.ProfileV3RetrofitApi
 import io.github.wykopmobilny.data.cache.api.AppCache
 import io.github.wykopmobilny.data.storage.api.AppStorage
-import io.github.wykopmobilny.domain.api.ApiClient
-import io.github.wykopmobilny.kotlin.AppDispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 internal class ProfilesRepository
     @Inject
     constructor(
-        private val api: ApiClient,
-        private val profilesApi: ProfileRetrofitApi,
+        private val profilesApiV3: ProfileV3RetrofitApi,
         private val appCache: AppCache,
         private val appStorage: AppStorage,
     ) {
         suspend fun blockUser(profileId: String) {
-            val response = api.mutation { profilesApi.block(profileId) }
-            update(profileId = profileId, response = response)
+            profilesApiV3.blockUser(profileId)
+            appStorage.blacklistQueries.insertOrReplaceProfile(profileId)
+            appCache.profileStateQueries.blockProfile(profileId)
         }
 
         suspend fun unblockUser(profileId: String) {
-            val response = api.mutation { profilesApi.unblock(profileId) }
-            update(profileId = profileId, response = response)
+            profilesApiV3.unblockUser(profileId)
+            appStorage.blacklistQueries.deleteProfile(profileId)
+            appCache.profileStateQueries.unblockProfile(profileId)
         }
 
         suspend fun observeUser(profileId: String) {
-            val response = api.mutation { profilesApi.observe(profileId) }
-            update(profileId = profileId, response = response)
+            profilesApiV3.observeUser(profileId)
+            appCache.profileStateQueries.observeProfile(profileId)
         }
 
         suspend fun unobserveUser(profileId: String) {
-            val response = api.mutation { profilesApi.unobserve(profileId) }
-            update(profileId = profileId, response = response)
-        }
-
-        private suspend fun update(
-            profileId: String,
-            response: ObserveStateResponse,
-        ) = withContext(AppDispatchers.IO) {
-            launch {
-                appStorage.blacklistQueries.run {
-                    if (response.isBlocked) {
-                        insertOrReplaceProfile(profileId)
-                    } else {
-                        deleteProfile(profileId)
-                    }
-                }
-            }
-            appCache.profileStateQueries.transaction {
-                if (response.isBlocked) {
-                    appCache.profileStateQueries.blockProfile(profileId)
-                } else {
-                    appCache.profileStateQueries.unblockProfile(profileId)
-                }
-                if (response.isObserved) {
-                    appCache.profileStateQueries.observeProfile(profileId)
-                } else {
-                    appCache.profileStateQueries.unobserveProfile(profileId)
-                }
-            }
+            profilesApiV3.unobserveUser(profileId)
+            appCache.profileStateQueries.unobserveProfile(profileId)
         }
     }
