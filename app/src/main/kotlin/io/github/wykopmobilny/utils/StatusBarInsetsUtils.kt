8582d24165a033a6ced80814a19b7998a41da438
Zarroc <starygwizd3@gmail.com>
2026-03-14 00:17:56 +0100

    Przywrócić top padding na toolbarach pod statusbarem

--- app/src/main/kotlin/io/github/wykopmobilny/utils/StatusBarInsetsUtils.kt
+++ app/src/main/kotlin/io/github/wykopmobilny/utils/StatusBarInsetsUtils.kt
@@ -1,6 +1,5 @@
 package io.github.wykopmobilny.utils
 
-import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -8,6 +7,11 @@ import androidx.core.view.WindowInsetsCompat
 /**
  * Ustawia gorny padding toolbara na rzeczywista wysokosc status bar
  * uzywajac WindowInsetsCompat API zamiast stalej wartosci 24dp.
+ *
+ * Wymaga android:fitsSystemWindows="true" na toolbarze w XML,
+ * aby view uczestniczyl w dystrybucji window insets.
+ * Listener nadpisuje domyslne zachowanie fitsSystemWindows
+ * i aplikuje wylacznie gorny inset status bar.
  */
 fun Toolbar.applyStatusBarInsets() {
     ViewCompat.setOnApplyWindowInsetsListener(this) { view, windowInsets ->
@@ -20,4 +24,5 @@ fun Toolbar.applyStatusBarInsets() {
         )
         windowInsets
     }
+    ViewCompat.requestApplyInsets(this)
 }
