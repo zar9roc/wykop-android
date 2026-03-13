322ef089da4c60ddfcfeb7a69f2eb9c18203bfda
Zarroc <starygwizd3@gmail.com>
2026-03-13 16:47:24 +0100

    Dodać przycisk wstecz w widoku znaleziska

--- ui/base/android/src/main/kotlin/io/github/wykopmobilny/utils/bindings/ToolbarBindings.kt
+++ ui/base/android/src/main/kotlin/io/github/wykopmobilny/utils/bindings/ToolbarBindings.kt
@@ -1,8 +1,14 @@
 package io.github.wykopmobilny.utils.bindings
 
+import android.util.TypedValue
 import androidx.activity.ComponentActivity
 import androidx.appcompat.widget.Toolbar
 
 fun Toolbar.bindBackButton(activity: ComponentActivity?) {
+    val typedValue = TypedValue()
+    val resolved = context.theme.resolveAttribute(android.R.attr.homeAsUpIndicator, typedValue, true)
+    if (resolved) {
+        setNavigationIcon(typedValue.resourceId)
+    }
     setNavigationOnClickListener { activity?.onBackPressedDispatcher?.onBackPressed() }
 }
